7d00b5768d026ed26a69b70a29da2a5e7414d841
arpad.varga90 <arpad.varga90@gmail.com>
2024-04-23 12:49:40 +0200

    build container section

--- static/index.js
+++ static/index.js
@@ -11,6 +11,23 @@ function createCustomElement(tag, cName = "", eText = "") {
   return customTag;
 }
 
+function buildCard({ title, image, text } = element) {
+  const cardRoot = createCustomElement("div", "card");
+  const cardHeader = createCustomElement("div", "card__header", title);
+  const cardBody = createCustomElement("div", "card__body");
+  const cardBodyImage = createCustomElement("div", "card__body__image");
+  const cardImage = createCustomElement("img");
+  cardImage.setAttribute("src", image);
+  cardImage.setAttribute("alt", title);
+  const cardBodyText = createCustomElement("div", "card__body__text", text);
+
+  cardBodyImage.append(cardImage);
+  cardBody.append(cardBodyImage, cardBodyText);
+  cardRoot.append(cardHeader,cardBody);
+
+  return cardRoot;
+}
+
 function main() {
   console.log(data);
   const rootEl = document.querySelector("#root");
@@ -24,6 +41,14 @@ function main() {
   );
   rootEl.append(introduct);
   introduct.append(h1);
+  
+  //Build container section
+  const container = createCustomElement("section", "container");
+  for (const key in data) {
+    const element = data[key];
+    container.append(buildCard(element));
+  }
+  rootEl.append(container);
 }
 
 main();
