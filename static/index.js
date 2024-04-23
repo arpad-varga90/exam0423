105ddd3a2534558a168950bcaf2d7a1cf69655ac
arpad.varga90 <arpad.varga90@gmail.com>
2024-04-23 12:09:06 +0200

    build introductory section

--- static/index.js
+++ static/index.js
@@ -1,7 +1,29 @@
-import data from './data.js';
+import data from "./data.js";
+
+function createCustomElement(tag, cName = "", eText = "") {
+  const customTag = document.createElement(tag);
+  if (cName !== "") {
+    customTag.className = cName;
+  }
+  if (eText !== "") {
+    customTag.innerText = eText;
+  }
+  return customTag;
+}
 
 function main() {
   console.log(data);
+  const rootEl = document.querySelector("#root");
+
+  //Build introductory section
+  const introduct = createCustomElement("section", "intro");
+  const h1 = createCustomElement(
+    "h1",
+    "intro__header",
+    "Visualized and clarified by AI"
+  );
+  rootEl.append(introduct);
+  introduct.append(h1);
 }
 
 main();
